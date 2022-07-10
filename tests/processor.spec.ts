1c68ded872b92a9cad62ebd73ff28dad6ce40625
Tipu Zayn <mailtokmahmed@gmail.com>
2022-07-10 16:02:18 +0530

    feat: Preserve CSS & Package Manager (#34)
    * feat: Preserve CSS & Package Manager
    
    * Version patch

--- tests/processor.spec.ts
+++ tests/processor.spec.ts
@@ -1,10 +1,15 @@
 import fs from 'fs-extra';
+import { CSS_STUB } from '../src/constants';
 import * as content from '../src/content';
 import * as helpers from '../src/helpers';
 import { handle } from '../src/processor';
 import { Framework } from '../src/types';
 
 describe('Processor', () => {
+  beforeAll(() => {
+    vi.stubGlobal('console', { ...console, log: vi.fn() });
+  });
+
   afterEach(() => {
     vi.clearAllMocks();
   });
@@ -13,8 +18,8 @@ describe('Processor', () => {
     vi.stubGlobal('console', { log: vi.fn() });
     vi.spyOn(fs, 'ensureFileSync').mockReturnValue();
     vi.spyOn(fs, 'readFileSync').mockReturnValue('');
-    vi.spyOn(fs, 'writeFileSync').mockReturnValue();
-    vi.spyOn(helpers, 'detectInstaller').mockReturnValue('npm');
+    const write = vi.spyOn(fs, 'writeFileSync').mockReturnValue();
+    vi.spyOn(helpers, 'installerPrefix').mockReturnValue('npm');
     const runner = vi
       .spyOn(helpers, 'runCommand')
       .mockReturnValue(Promise.resolve());
@@ -31,11 +36,43 @@ describe('Processor', () => {
       steps: [step],
     };
 
-    await handle(framework);
+    await handle(framework, {});
 
     expect(runner).toHaveBeenCalledWith('npm deps');
     expect(runner).toHaveBeenCalledWith('init');
     expect(setup).toHaveBeenCalledWith(framework);
     expect(step).toHaveBeenCalled();
+    expect(write).toHaveBeenCalledWith('css', CSS_STUB);
+  });
+
+  it('will process a framework driver with replace', async () => {
+    vi.stubGlobal('console', { log: vi.fn() });
+    vi.spyOn(fs, 'ensureFileSync').mockReturnValue();
+    vi.spyOn(fs, 'readFileSync').mockReturnValue('existing');
+    const write = vi.spyOn(fs, 'writeFileSync').mockReturnValue();
+    vi.spyOn(helpers, 'installerPrefix').mockReturnValue('npm');
+    const runner = vi
+      .spyOn(helpers, 'runCommand')
+      .mockReturnValue(Promise.resolve());
+    const setup = vi.spyOn(content, 'setupContent').mockResolvedValue();
+    const step = vi.fn().mockResolvedValue(true);
+    const framework: Framework = {
+      content: {
+        name: 'content',
+        files: [],
+      },
+      requiredDependencies: ['deps'],
+      initCommands: ['init'],
+      cssLocation: 'css',
+      steps: [step],
+    };
+
+    await handle(framework, { preserve: true });
+
+    expect(runner).toHaveBeenCalledWith('npm deps');
+    expect(runner).toHaveBeenCalledWith('init');
+    expect(setup).toHaveBeenCalledWith(framework);
+    expect(step).toHaveBeenCalled();
+    expect(write).toHaveBeenCalledWith('css', 'existing\n\n' + CSS_STUB);
   });
 });
