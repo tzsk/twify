d8a08d356b1ae3aa17e27fa1b7682ae0f51b81b6
Kazi Ahmed <mailtokmahmed@gmail.com>
2023-01-20 17:40:52 +0530

    add: Support for Next 13 (#242)

--- src/frameworks/nextjs.ts
+++ src/frameworks/nextjs.ts
@@ -7,6 +7,8 @@ const NextJS: Framework = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   steps: [],
 };
